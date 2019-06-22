23bee684138f36fc2d41e15e52af73b5496c0411
Ilya <polushan@yandex.ru>
2019-06-22 21:10:27 +0400

    Initial commit

---
+++ src/test/java/com/pingpong/pingservice/PingServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pingpong.pingservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PingServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
