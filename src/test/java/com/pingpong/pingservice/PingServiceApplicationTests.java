2200ba29981c05ac9e692171c8a6bdcdfb033b7e
Ilya <polushan@yandex.ru>
2019-06-24 17:49:18 +0400

    Add test properties

--- src/test/java/com/pingpong/pingservice/PingServiceApplicationTests.java
+++ src/test/java/com/pingpong/pingservice/PingServiceApplicationTests.java
@@ -3,10 +3,12 @@ package com.pingpong.pingservice;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(properties = {"pong.host=localhost", "vcap.application.uris[0]=test"})
 public class PingServiceApplicationTests {
 
 	@Test
