673fac25939978d62d54656341d93acb57011012
Ilya <polushan@yandex.ru>
2019-06-29 01:09:38 +0400

    Add config server

--- src/main/java/com/pingpong/pingservice/PingServiceApplication.java
+++ src/main/java/com/pingpong/pingservice/PingServiceApplication.java
@@ -3,10 +3,12 @@ package com.pingpong.pingservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@RefreshScope
 @EnableSwagger2
 @EnableEurekaClient
 @EnableFeignClients
